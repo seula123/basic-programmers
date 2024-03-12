875db369dfc8daf72aa63e448d77dd74d4af2df9
seula123 <sla6239@gamil.com>
2024-03-13 02:05:03 +0900

    20230313

---
+++ src/main/java/no1_no20/문자열_출력하기.java
@@ -0,0 +1,13 @@
+package no1_no20;
+
+import java.util.Scanner;
+
+public class 문자열_출력하기 {
+
+    public class Solution {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            String a = sc.next();
+            System.out.println(a);
+        }
+}
