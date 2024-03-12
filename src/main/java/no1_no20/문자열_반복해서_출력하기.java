980bbe928d9eadab271a6a0810fd3dc3b806f44a
seula123 <sla6239@gamil.com>
2024-03-13 02:02:43 +0900

    20230313

---
+++ src/main/java/no1_no20/문자열_반복해서_출력하기.java
@@ -0,0 +1,18 @@
+package no1_no20;
+
+import java.util.Scanner;
+
+public class 문자열_반복해서_출력하기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int n = sc.nextInt();
+        String a = "";
+
+        for(int i = 0; i<n; i++){
+            a += str;
+        }
+
+        System.out.println(a);
+    }
+}
