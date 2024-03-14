62b1b78734f7c8a18b20c2b62c59b1b059a4a0d5
seula123 <sla6239@gamil.com>
2024-03-14 16:03:48 +0900

    20230314

---
+++ src/main/java/no1_no20/대소문자_바꿔서_출력하기.java
@@ -0,0 +1,24 @@
+package no1_no20;
+
+import java.util.Scanner;
+
+public class 대소문자_바꿔서_출력하기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String answer = "";
+
+        for(int i = 0; i<a.length(); i++){
+            char c = a.charAt(i);
+
+            if(Character.isUpperCase(c)){
+                answer += Character.toLowerCase(c);
+            } else {
+                answer += Character.toUpperCase(c);
+            }
+
+        }
+
+        System.out.println(answer);
+    }
+}
---
+++ src/main/java/no1_no20/특수문자_출력하기.java
@@ -0,0 +1,9 @@
+package no1_no20;
+
+public class 특수문자_출력하기 {
+    public static void main(String[] args) {
+
+        System.out.println("!@#$%^&*(\\'\"<>?:;");
+
+    }
+}
